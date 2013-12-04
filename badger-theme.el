b1cff1e82e7772c2d9930681445cee18aa78d3b9
Cody Canning <cocanning11@gmail.com>
2013-12-03 21:03:51 -0500

    some font-lock changes

--- badger-theme.el
+++ badger-theme.el
@@ -1,13 +1,7 @@
 ;;; badger-theme.el --- A theme for Emacs 24
 
 ;;; Commentary:
-;;; Based loosely on both the wombat and tomorrow themes.
-
-;;; Currently supports:
-;;; - [X] font-lock
-;;; - [ ] 
-;;; - [X] eshell 
-;;; - [X] org-mode
+;;; Based loosely on both the wombat and tomorrow (Base16) themes.
 
 ;;; Credits: 
 ;;; I followed Bozhidar Batsov's style in zenburn.el, and used his macro.
@@ -18,33 +12,39 @@
 
 ;;; Badger Color Pallette
 (defvar badger-colors-alist
-  '(("badger-blue"     . "#8AC6F2")
-    ("badger-fg"       . "#F6F3E8")
-    ("badger-bg"       . "#1C1C1C")
-    ("badger-bg-1"     . "#171717")
-    ("badger-charcoal" . "#656868")
-    ("badger-salmon"   . "#F28B86")
-    ("badger-violet"   . "#BF93C3")
-    ("badger-orange"   . "#EA9847")
-    ("badger-green"    . "#86B187")
-    ("badger-lime"     . "#84C452")
-    ("badger-yellow"   . "#EDEB71")
-    ("badger-fg+1"     . "#FBF9F3")
-    ("badger-teal"     . "#65A399")
-    ("badger-pink"     . "#E18CBB")
-    ("badger-brown"    . "#AC8952")
-    ("badger-red"      . "#E2434C")
-    ("badger-dull-red" . "#A55662")
-    
-;;    ("badger-bg-1"     . "#170C0C")
-    ("badger-bg-05"    . "#1B1B1B")
-    ("badger-bg+1"     . "#353535")
-    ("badger-dark-violet" . "#635770")
-    ("badger-darker-violet"  .   "#433F4F")
-    ("badger-dv-invert"   . "#9AA68E")
-    ("badger-link"       .  "#8ACDAA")
-    ("badger-warn"     . "magenta")
-    ("badger-succ"     . "cyan")
+  '(
+    ("badger-fg"            . "#F6F3E8")
+    ("badger-fg+1"          . "#FBF9F3")
+    ("badger-bg"            . "#1C1C1C")
+    ("badger-bg-1"          . "#171717")
+;;  ("badger-bg-1"          . "#170C0C")
+    ("badger-bg-05"         . "#1B1B1B")
+    ("badger-bg+1"          . "#353535")
+
+;;; Primary Hues
+    ("badger-blue"          . "#8AC6F2")
+    ("badger-charcoal"      . "#656868")
+    ("badger-salmon"        . "#F28B86")
+    ("badger-violet"        . "#BF93C3")
+    ("badger-orange"        . "#EA9847")
+    ("badger-green"         . "#86B187")
+    ("badger-yellow"        . "#E0D063")
+
+;;; Secondary Hues
+    ("badger-lime"          . "#84C452")
+    ("badger-teal"          . "#65A399")
+    ("badger-pink"          . "#E18CBB")
+    ("badger-brown"         . "#AC8952")
+    ("badger-red"           . "#E2434C")
+    ("badger-dull-red"      . "#A55662")
+    ("badger-dark-violet"   . "#635770")
+    ("badger-darker-violet" . "#433F4F")
+    ("badger-olive"         . "#9AA68E")
+
+;;; Misc.
+    ("badger-link"          .  "#8ACDAA")
+    ("badger-warn"          . "magenta")
+    ("badger-succ"          . "cyan")
     )
   "List of Badger colors.
 Each element has the form (NAME . HEX).")
@@ -68,8 +68,7 @@ Also bind `class' to ((class color) (min-colors 89))."
 ;; >>>>> Built-in
    '(button ((t (:underline t))))
    `(link ((t (:bold t :foreground ,badger-blue :underline t :weight bold))))
-
-;;   `(link-visited ((t (:foreground ,badger-salmon-2 :underline t :weight normal))))
+;; `(link-visited ((t (:foreground ,badger-salmon-2 :underline t :weight normal))))
 
    ;; ordinary text. Its background color is used as the frame's background color. 
    `(default ((t (:foreground ,badger-fg :background ,badger-bg))))
@@ -100,7 +99,6 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(region ((t (:background ,badger-charcoal))))
 
 ;; >>>>> mode-line
-;;   `(mode-line ((t (:background ,badger-dk :foreground ,"gray"))))
    `(mode-line    ((,class (:foreground ,badger-charcoal
                                         :background ,"black"
                                         :box (:line-width -1 :style released-button)))
@@ -108,7 +106,7 @@ Also bind `class' to ((class color) (min-colors 89))."
 
    `(mode-line-inactive ((t (:background ,badger-bg+1 :foreground ,"black" :box nil))))
    `(mode-line-buffer-id ((t (:foreground ,badger-salmon))))
-   `(minibuffer-prompt ((t (:foreground ,badger-lime))))
+   `(minibuffer-prompt ((t (:foreground ,badger-violet))))
 ;;   `(mode-line-highlight ((t (:foreground ,badger-lime))))
 
 ;; >>>>> powerline
@@ -119,13 +117,13 @@ Also bind `class' to ((class color) (min-colors 89))."
 
 ;; >>>>> font-lock
    `(font-lock-keyword-face ((t (:foreground ,badger-blue))))                    
-   `(font-lock-builtin-face ((t (:foreground ,badger-orange))))                  
+   `(font-lock-builtin-face ((t (:foreground ,badger-salmon))))                  
    `(font-lock-comment-face ((t (:foreground ,badger-charcoal))))
-   `(font-lock-function-name-face ((t (:foreground ,badger-salmon :weight normal)))) 
+   `(font-lock-function-name-face ((t (:foreground ,badger-orange :weight normal)))) 
    `(font-lock-string-face ((t (:foreground ,badger-green))))
    `(font-lock-variable-name-face ((t (:foreground ,badger-violet))))
    `(font-lock-type-face ((t (:foreground ,badger-yellow))))   
-   `(font-lock-constant-face ((t (:foreground ,badger-lime))))
+   `(font-lock-constant-face ((t (:foreground ,badger-salmon))))
    `(font-lock-warning-face ((t (:foreground ,badger-yellow :weight bold))))
 
 ;; >>>>> eshell 
@@ -142,10 +140,10 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(eshell-ls-symlink ((t (:foreground ,badger-link :weight bold))))
 
 ;; >>>>> Org mode
-   `(org-document-info-keyword ((t (:foreground ,badger-dv-invert))))
+   `(org-document-info-keyword ((t (:foreground ,badger-olive))))
    `(org-document-title ((t (:foreground ,badger-salmon :height 1.50))))
    `(org-archived ((t (:foreground ,badger-fg :weight bold))))
-   `(org-checkbox ((t (:foreground ,badger-fg+1 :foreground ,badger-dv-invert 
+   `(org-checkbox ((t (:foreground ,badger-fg+1 :foreground ,badger-olive 
                                    :box (:line-width 1 :style released-button)))))
    `(org-done ((t (:foreground ,badger-lime :strike-through t))))
    `(org-todo ((t (:foreground ,badger-red))))
@@ -174,16 +172,24 @@ Also bind `class' to ((class color) (min-colors 89))."
    ;; `(org-time-grid ((t (:foreground ,zenburn-orange))))
    ;; `(org-upcoming-deadline ((t (:inherit font-lock-keyword-face))))
 
-   `(org-special-keyword ((t (:foreground ,badger-dv-invert :weight normal))))
-   `(org-table ((t (:foreground ,badger-dv-invert))))
+   `(org-special-keyword ((t (:foreground ,badger-olive :weight normal))))
+   `(org-table ((t (:foreground ,badger-olive))))
    `(org-tag ((t (:bold t :foreground ,badger-orange :strike-through nil))))
    `(org-warning ((t (:bold t :foreground ,badger-pink :weight bold))))
    `(org-column ((t (:background ,badger-bg-1))))
    `(org-column-title ((t (:background ,badger-bg-1 :foreground ,badger-lime :underline t))))
    `(org-mode-line-clock ((t (:foreground ,badger-yellow))))
    `(org-footnote ((t (:foreground ,badger-link :underline t))))
-   `(org-code ((t (:foreground ,badger-dv-invert))))
+   `(org-code ((t (:foreground ,badger-olive))))
    `(org-verbatim ((t (:inherit org-code))))
+
+;; >>>>> auto-complete
+   ;; `(ac-candidate-face ((t (:background ,zenburn-bg+3 :foreground ,zenburn-bg-2))))
+   ;; `(ac-selection-face ((t (:background ,zenburn-blue-4 :foreground ,zenburn-fg))))
+   ;; `(popup-tip-face ((t (:background ,zenburn-yellow-2 :foreground ,zenburn-bg-2))))
+   ;; `(popup-scroll-bar-foreground-face ((t (:background ,zenburn-blue-5))))
+   ;; `(popup-scroll-bar-background-face ((t (:background ,zenburn-bg-1))))
+   ;; `(popup-isearch-match ((t (:background ,zenburn-bg :foreground ,zenburn-fg))))
 
    ))
 
